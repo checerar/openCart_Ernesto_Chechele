9c6a8c94793bf0cae07ad018cbe9067f5bb76a19
checerar <eechechele@gmail.com>
2023-06-12 16:14:00 +0200

    last updates

--- src/test/java/StepDefs/CartStepDefs.java
+++ src/test/java/StepDefs/CartStepDefs.java
@@ -13,33 +13,26 @@ public class CartStepDefs {
     CartPage cartPage = PagesFactory.getInstance().getCartPage();
     @When("the user clicks on the {string} button of a {string}")
     public void clickAddToCartButton(String button, String product) {
-        // Implementa aquí la lógica para hacer clic en el botón "Add to Cart" del producto indicado
     }
 
     @Then("the {string} should be added to the shopping cart")
     public void verifyProductAddedToCart(String product) {
-        // Implementa aquí la lógica para verificar que el producto indicado se haya añadido al carrito de compras
     }
 
 
     @Given("the user has added a {string} to the shopping cart")
     public void userHasAddedProductToCart(String product) {
-        // Implementa aquí la lógica para simular que el usuario ha agregado el producto indicado al carrito de compras
     }
-
     @When("the user clicks on the shopping cart icon")
     public void clickShoppingCartIcon() {
-        // Implementa aquí la lógica para hacer clic en el icono del carrito de compras
     }
 
     @When("clicks on the {string} button next to the {string}")
     public void clickRemoveButton(String button, String product) {
-        // Implementa aquí la lógica para hacer clic en el botón "Remove" junto al producto indicado
     }
 
     @Then("the {string} should be removed from the shopping cart")
     public void verifyProductRemovedFromCart(String product) {
-        // Implementa aquí la lógica para verificar que el producto indicado se haya eliminado del carrito de compras
     }
 
     @Given("the user is on the {string} page")
