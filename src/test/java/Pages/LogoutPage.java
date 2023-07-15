6a596bf6f890e7dbbfc366b479ed334302d5af3a
checerar <eechechele@gmail.com>
2023-07-15 19:27:33 +0200

    Refactored code and locators

--- src/test/java/Pages/LogoutPage.java
+++ src/test/java/Pages/LogoutPage.java
@@ -1,27 +1,46 @@
 package Pages;
 
-import org.openqa.selenium.WebDriver;
+import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.PageFactory;
 
-public class LogoutPage extends AbstractPage{
-    private WebDriver driver;
+public class LogoutPage extends AbstractPage {
 
-    // Locators
-    private By successMessage = By.id("content");
+    private final By myAccount = By.xpath("//span[@class='caret']");
+    private final By clickLogout = By.xpath("//ul/li/a[contains(text(), 'Logout')]");
+    private final By successMessage = By.xpath("//p[contains(text(), 'You have been logged off your account. It is now safe to leave the computer.')]");
 
-    public LogoutPage(WebDriver driver) {
+    public LogoutPage(WebDriver driver) throws InterruptedException {
         super(driver);
-        PageFactory.initElements(driver, this);
     }
 
     @Override
     public WebElement getPageLoadedTestElement() {
-        return null;
+        return getDriver().findElement(myAccount);
     }
 
-    public String getSuccessMessage() {
-        return driver.findElement(successMessage).getText();
+    public void clickMenu(String menuText)  {
+        getDriver().findElement(myAccount).click();
+    }
+
+    public void clickOption(String optionText) {
+        getDriver().findElement(clickLogout).click();
+    }
+
+    public void verifyRedirectToLogoutPage() {
+        String currentUrl = getDriver().getCurrentUrl();
+        Assert.assertEquals("https://opencart.abstracta.us/index.php?route=account/logout", currentUrl);
+    }
+
+    public void verifySuccessMessage(String expectedMessage) {
+        String actualMessage = getDriver().findElement(successMessage).getText();
+        Assert.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    public void navigateToDifferentPage(String url) {
+        getDriver().get(url);
+        String currentUrl = getDriver().getCurrentUrl();
+        Assert.assertEquals(url, currentUrl);
     }
 }
