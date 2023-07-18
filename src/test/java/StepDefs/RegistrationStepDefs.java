7d781e959275c1077817a8f95d2bc546b8699b2d
checerar <eechechele@gmail.com>
2023-07-18 23:00:29 +0200

    Fixed step defs

--- src/test/java/StepDefs/RegistrationStepDefs.java
+++ src/test/java/StepDefs/RegistrationStepDefs.java
@@ -1,57 +1,71 @@
 package StepDefs;
-import Pages.LoginPage;
-import Pages.PagesFactory;
+
 import Pages.RegistrationPage;
-import org.openqa.selenium.WebDriver;
-import io.cucumber.java.en.Given;
+import Pages.PagesFactory;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
 
 public class RegistrationStepDefs {
     RegistrationPage registrationPage = PagesFactory.getInstance().getRegistrationPage();
-
-    @Given("the user is on the home page")
-    public void userIsOnHomePage() {
+    @And("the user is on the registration page")
+    public void RegistrationPage() {
+        registrationPage.navigateToRegistrationPage();
     }
+    @When("the user fills in all the mandatory fields with valid information")
+    public void fillRegistrationForm() {
+        String firstName = registrationPage.getRandomFirstName();
+        String lastName = registrationPage.getRandomLastName();
+        String email = registrationPage.getRandomEmail();
+        String telephone = registrationPage.getRandomTelephone();
+        String password = registrationPage.getRandomPassword();
+        String passwordConfirm = password;
 
-    @Given("clicks on My Account")
-    public void clickOnMyAccount() {
-        }
-
-    @Given("clicks on Register option")
-    public void clickOnRegisterOption() {
-
+        registrationPage.fillRegistrationForm(firstName, lastName, email, telephone, password, passwordConfirm);
     }
-
-    @When("the user fills in all the mandatory {string} with {string}")
-    public void fillInMandatoryFields(String field, String value) {
-    }
-
-    @When("clicks in the checkbox of the Privacy Policy")
-    public void clickPrivacyPolicyCheckbox() {
-    }
-
-    @When("clicks on the {string} button")
-    public void clickContinueButton(String button) {
+    @And("clicks on the checkbox of the Privacy Policy")
+    public void PrivacyPolicy() {
+        registrationPage.clickPrivacyPolicy();
     }
 
     @Then("the user should receive a success message indicating successful registration")
-    public void verifySuccessMessage() {
+    public void SuccessfulRegistration() {
+        Assert.assertTrue("Successful registration message not displayed", registrationPage.isSuccessfulRegistrationMessageDisplayed());
+    }
+
+    @When("the user fills in the registration form with missing information")
+    public void fillRegistrationFormWithMissingInformation() {
+        registrationPage.fillRegistrationForm("", "", "", "", "", "");
+    }
+    @And("the user enters an existing email: {string}")
+    public void enterExistingEmail(String existingEmail) {
+        registrationPage.clearEmailField();
+        registrationPage.setEmail(existingEmail);
+    }
+
+    @Then("the user should see an error message {string}")
+    public void verifyErrorMessage(String errorMessage) {
+        registrationPage.verifyErrorMessage(errorMessage);
+    }
+
+    @When("the user fills in all the mandatory fields with valid information without accepting the  privacy policy")
+    public void validInformationWithoutAcceptingPrivacyPolicy() {
+        fillRegistrationForm();
+    }
+    @Then("the user should see the field {string} error message {string}")
+    public void verifyFieldErrorMessage(String field, String expectedErrorMessage) {
+        String actualErrorMessage = registrationPage.getErrorMessage(field);
+        Assert.assertEquals("Error message for field " + field + " does not match", expectedErrorMessage, actualErrorMessage);
     }
 
     @Then("the user should see an error message")
-    public void verifyErrorMessage() {
+    public void theUserShouldSeeAnErrorMessage() {
+        Assert.assertTrue("Error message not displayed: " + "Warning: You must agree to the Privacy Policy!", registrationPage.isErrorMessageDisplayed("Warning: You must agree to the Privacy Policy!"));
     }
 
-    @Then("the user should see an error message indicating that they must agree to the Privacy Policy")
-    public void verifyPrivacyPolicyErrorMessage() {
-    }
 
-    @Then("the user should see an error message indicating that the E-Mail Address is already registered")
-    public void verifyExistingEmailErrorMessage() {
-    }
-
-    @When("the user fills in any mandatory {string} with missing {string} information")
-    public void theUserFillsInAnyMandatoryWithMissingInformation(String arg0, String arg1) {
-    }
 }
+
+
+
